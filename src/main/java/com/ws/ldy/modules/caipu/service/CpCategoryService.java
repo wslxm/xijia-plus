eb8d55058f5a8683584dbcd731cc83343c52ad02
“wangsong” <1720696548@qq.com>
2020-10-05 00:23:08 +0800

    --

---
+++ src/main/java/com/ws/ldy/modules/caipu/service/CpCategoryService.java
@@ -0,0 +1,62 @@
+package com.ws.ldy.modules.caipu.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ws.ldy.modules.caipu.model.entity.CpCategory;
+import com.ws.ldy.modules.caipu.model.vo.CpCategoryVO;
+
+import java.util.List;
+
+/**
+ * 菜谱类别字典表
+ * <p>
+ *  ::本代码由[兮家小二]提供的代码生成器生成,如有问题,请手动修改 ::作者CSDN:https://blog.csdn.net/qq_41463655 
+ * </p>
+ * @author  wangsong
+ * @email  1720696548@qq.com
+ * @date  2020-10-04 21:44:55
+ */
+public interface CpCategoryService extends IService<CpCategory> {
+
+    /**
+     *
+     * @param
+     * @return
+     */
+    List<CpCategoryVO> findTree();
+
+
+    /**
+     *  根据code查询数据+ 下级所有层级数据（无限递归），不包括禁用数据,数据Sort排序，在根据 Code排序
+     *
+     * @param code
+     * @param isDisable 是否查询禁用数据
+     * @return java.util.List<com.ws.ldy.admin.model.vo.AdminDictionaryVO>
+     * @date 2020/7/12 0012 19:22
+     */
+    CpCategoryVO findByCodeFetchDictVO(String code, boolean isDisable);
+
+    /**
+     * 查询下级所有Id, 包括禁用数据
+     */
+    List<String> findByIdFetchIds(String id);
+
+//    /**
+//     * 分组查询-key-value数据： 不包括禁用数据
+//     * @return
+//     */
+//    Map<String, AdminDictionaryVO.FindCodeGroup> findCodeGroup();
+
+
+//    /**
+//     * 拼接java 枚举类
+//     * @param dict
+//     */
+//    String generateEnumJava(CpCategoryVO dict);
+//
+//    /**
+//     * 拼接js 字典数据
+//     * @param dict
+//     */
+//    String generateEnumJs(CpCategoryVO dict);
+}
+
