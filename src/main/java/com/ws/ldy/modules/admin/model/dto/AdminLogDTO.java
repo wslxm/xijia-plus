e54b3c15ca67860e042c13b7f26411308c99b8ad
“wangsong” <1720696548@qq.com>
2020-10-29 13:38:40 +0800

    - 22 登录/授权 修改为aop 统一处理 - 23 aop 中增加 log 操作记录到数据库,比较完整, 请求来源，请求数据，ip，返回数据，成功失败等, 这也是登录/授权 修改到aop的主要原因之一

---
+++ src/main/java/com/ws/ldy/modules/admin/model/dto/AdminLogDTO.java
@@ -0,0 +1,97 @@
+package com.ws.ldy.modules.admin.model.dto;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.ws.ldy.others.base.model.BaseDto;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.ToString;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * 操作记录表
+ * <p>
+ *  ::本代码由[兮家小二]提供的代码生成器生成,如有问题,请手动修改 ::作者CSDN:https://blog.csdn.net/qq_41463655 
+ * </p>
+ * @author  wangsong
+ * @email  1720696548@qq.com
+ * @date  2020-10-28 20:44:32
+ */
+@Data
+@ToString(callSuper = true)
+@ApiModel(value = "AdminLogDTO 对象", description = "操作记录表")
+public class AdminLogDTO extends BaseDto {
+
+    private static final long serialVersionUID = 0L;
+    
+    @ApiModelProperty(notes = "请求人" ,position = 0)
+    private String fullName;
+
+    @ApiModelProperty(notes = "请求人Id (根据端来区分)" ,position = 1)
+    private String userId;
+
+    @ApiModelProperty(notes = "请求终端(字典code, 如 0-管理端 1-用户端 -1-All)" ,position = 2)
+    private Integer type;
+
+    @ApiModelProperty(notes = "请求来源" ,position = 3)
+    private String referer;
+
+    @ApiModelProperty(notes = "请求url" ,position = 4)
+    private String url;
+
+    @ApiModelProperty(notes = "请求uri" ,position = 5)
+    private String uri;
+
+    @ApiModelProperty(notes = "用户真实Ip" ,position = 6)
+    private String ip;
+
+    @ApiModelProperty(notes = "用户主机名" ,position = 7)
+    private String host;
+
+    @ApiModelProperty(notes = "请求方式(post-get)" ,position = 8)
+    private String method;
+
+    @ApiModelProperty(notes = "服务器地址" ,position = 9)
+    private String serverName;
+
+    @ApiModelProperty(notes = "服务器端口" ,position = 10)
+    private String port;
+
+    @ApiModelProperty(notes = "请求包" ,position = 11)
+    private String packageName;
+
+    @ApiModelProperty(notes = "请求类" ,position = 12)
+    private String className;
+
+    @ApiModelProperty(notes = "请求类--swagger注释" ,position = 13)
+    private String classDesc;
+
+    @ApiModelProperty(notes = "请求方法--swagger注释" ,position = 14)
+    private String methodDesc;
+
+    @ApiModelProperty(notes = "请求数据" ,position = 15)
+    private String requestData;
+
+    @ApiModelProperty(notes = "返回数据" ,position = 16)
+    private String responseData;
+
+    @ApiModelProperty(notes = "1-请求成功 0-请求失败" ,position = 17)
+    private Integer state;
+
+    @ApiModelProperty(notes = "程序响应总耗时" ,position = 18)
+    private Long executeTime;
+
+    @ApiModelProperty(notes = "业务执行总耗时" ,position = 18)
+    private Long businessTime;
+}
+
---
+++ src/main/java/com/ws/ldy/modules/admin/service/impl/AdminLogServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ws.ldy.modules.admin.service.impl;
+
+import com.ws.ldy.modules.admin.mapper.AdminLogMapper;
+import com.ws.ldy.modules.admin.model.entity.AdminLog;
+import com.ws.ldy.modules.admin.service.AdminLogService;
+import com.ws.ldy.others.base.service.impl.BaseIServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * 操作记录表
+ * <p>
+ *  ::本代码由[兮家小二]提供的代码生成器生成,如有问题,请手动修改 ::作者CSDN:https://blog.csdn.net/qq_41463655 
+ * </p>
+ * @author  wangsong
+ * @email  1720696548@qq.com
+ * @date  2020-10-28 20:44:32
+ */
+@Service
+public class AdminLogServiceImpl extends BaseIServiceImpl<AdminLogMapper, AdminLog> implements AdminLogService {
+
+
+}
