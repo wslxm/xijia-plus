35140f6cd628c2a392a241234b360c73ff705099
“wangsong” <1720696548@qq.com>
2020-12-20 21:15:58 +0800

    --

---
+++ src/main/java/com/ws/ldy/modules/file/service/impl/XjFileServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ws.ldy.modules.file.service.impl;
+
+import com.ws.ldy.modules.file.mapper.XjFileMapper;
+import com.ws.ldy.modules.file.model.entity.XjFile;
+import com.ws.ldy.modules.file.service.XjFileService;
+import com.ws.ldy.others.base.service.impl.BaseIServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * 常用工具文件管理
+ * <p>
+ *  ::本代码由[兮家小二]提供的代码生成器生成,如有问题,请手动修改 ::作者CSDN:https://blog.csdn.net/qq_41463655 
+ * </p>
+ * @author  wangsong
+ * @email  1720696548@qq.com
+ * @date  2020-12-17 16:21:46
+ */
+@Service
+public class XjFileServiceImpl extends BaseIServiceImpl<XjFileMapper, XjFile> implements XjFileService {
+
+
+}
