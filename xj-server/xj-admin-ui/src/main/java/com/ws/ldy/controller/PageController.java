7b59b8633c2edfa4cd08221fab19e64fd74b2674
“wangsong” <1720696548@qq.com>
2021-01-23 18:47:54 +0800

    --模块化

--- xj-server/xj-admin-ui/src/main/java/com/ws/ldy/controller/PageController.java
+++ xj-server/xj-admin-ui/src/main/java/com/ws/ldy/controller/PageController.java
@@ -2,6 +2,8 @@ package com.ws.ldy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 通用页面跳转Controller,主要用于所有的页面跳转（添加页、修改页/查询页/等跳转）
@@ -15,14 +17,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class PageController {
 
-   /**
-    * 页面跳转,跳转对应templates的目录下的html页面,如：/page/admin_menu_menu 跳转到 admin/menu/menu.html,每一层的目录用_分隔
-    * @author wangsong
-    * @param fileName
-    * @date 2021/1/23 0023 16:33
-    * @return java.lang.String
-    * @version 1.0.0
-    */
+    /**
+     * 页面跳转,跳转对应templates的目录下的html页面,如：/page/admin_menu_menu 跳转到 admin/menu/menu.html,每一层的目录用_分隔
+     * @author wangsong
+     * @param fileName
+     * @date 2021/1/23 0023 16:33
+     * @return java.lang.String
+     * @version 1.0.0
+     */
+    @RequestMapping(value = "/page/{fileName}", method = RequestMethod.GET)
     public String add(@PathVariable String fileName) {
         String url = fileName.replace("_", "/");
         return url;
