2fe0fd99a01d4a4c0d1cf4996de8a35d6897ef4c
“wangsong” <1720696548@qq.com>
2022-06-10 17:37:10 +0800

    更新线程池工具+事务工具

---
+++ spring-boot-plus2-demo/src/main/java/com/test/springbootplus2/manage/test/controller/TransactionTestController.java
@@ -0,0 +1,56 @@
+package com.test.springbootplus2.manage.test.controller;
+
+import io.github.wslxm.springbootplus2.core.config.threadpool.XjThreadUtil;
+import io.github.wslxm.springbootplus2.core.constant.BaseConstant;
+import io.github.wslxm.springbootplus2.core.result.R;
+import io.github.wslxm.springbootplus2.core.utils.XjTransactionUtil;
+import io.github.wslxm.springbootplus2.manage.admin.model.entity.AdminUser;
+import io.github.wslxm.springbootplus2.manage.admin.service.AdminUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 异步工具类测试
+ *
+ * @author wangsong
+ * @email 1720696548@qq.com
+ * @date 2022/6/10 14:49
+ */
+@RestController
+@RequestMapping(BaseConstant.Uri.API_OPEN + "/xj/transaction")
+@Api(value = "TransactionTestController", tags = "事务执行后执行")
+@Slf4j
+public class TransactionTestController {
+
+	@Autowired
+	private AdminUserService adminUserService;
+
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	@ApiOperation(value = "事务执行后执行测试")
+	// 添加事务注解
+	@Transactional(rollbackFor = Exception.class)
+	public R<Boolean> test() {
+		log.info("1、进入方法");
+
+		// 查询数据模拟
+		List<AdminUser> list = adminUserService.list();
+
+		XjTransactionUtil.runOfAfterCommit(() -> {
+			log.info("3、我开始执行了");
+		});
+
+		// 延时3秒
+		XjThreadUtil.sleep(3000);
+		log.info("2、方法执行完毕");
+		return R.success(true);
+	}
+}
