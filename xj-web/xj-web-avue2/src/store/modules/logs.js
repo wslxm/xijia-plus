8115ca6789b7aaf68fae9273db85ccb78fdeb3ed
“wangsong” <1720696548@qq.com>
2022-08-31 19:41:49 +0800

    目录更新

--- xj-web/xj-web-avue2/src/store/modules/logs.js
+++ xj-web/xj-web-avue2/src/store/modules/logs.js
@@ -1,6 +1,5 @@
 import { setStore, getStore } from '@/util/store'
 import dayjs from 'dayjs'
-import { sendLogs } from '@/api/user'
 const logs = {
   state: {
     logsList: getStore({ name: 'logsList' }) || [],
@@ -9,12 +8,12 @@ const logs = {
     //发送错误日志
     SendLogs ({ state, commit }) {
       return new Promise((resolve, reject) => {
-        sendLogs(state.logsList).then(() => {
-          commit('CLEAR_LOGS');
-          resolve();
-        }).catch(error => {
-          reject(error)
-        })
+        // sendLogs(state.logsList).then(() => {
+        //   commit('CLEAR_LOGS');
+        //   resolve();
+        // }).catch(error => {
+        //   reject(error)
+        // })
       })
     },
   },
@@ -28,7 +27,7 @@ const logs = {
         message,
         stack,
         info: info.toString()
-      }))
+      }));
       setStore({ name: 'logsList', content: state.logsList })
     },
     CLEAR_LOGS: (state) => {
--- xj-web/xj-web-avue2/src/store/modules/user.js
+++ xj-web/xj-web-avue2/src/store/modules/user.js
@@ -1,9 +1,8 @@
 import {removeToken, setToken} from '@/util/auth'
 import {getStore, setStore} from '@/util/store'
 import {deepClone} from '@/util/util'
-import {getMenu, getTopMenu, getUserInfo, loginByUsername, refeshToken} from '@/api/user'
-import {formatPath} from '@/router/avue-router'
-
+import {formatPath} from '@/router/avue-router';
+import crud from '@/util/crud'
 const user = {
     state: {
         userInfo: {},
@@ -15,65 +14,48 @@ const user = {
         token: getStore({name: 'token'}) || '',
     },
     actions: {
-
-        //根据用户名登录
+        // 根据用户名登录
         LoginByUsername({commit}, userInfo = {}) {
-            console.log("---");
-            // const user = encryption({
-            //   data: userInfo,
-            //   type: 'Aes',
-            //   key: 'avue',
-            //   param: ['useranme', 'password']
-            // });
-            let password = Base64.encode(userInfo.password);
-            //Base64.decode('abcdefg');
             return new Promise((resolve) => {
-                loginByUsername(userInfo.username, password, userInfo.code, userInfo.redomStr).then(res => {
+                let password = Base64.encode(userInfo.password);
+                crud.post("/api/admin/sys/user/login", {
+                    username: userInfo.username,
+                    password: password,
+                    code: userInfo.code,
+                    redomStr: userInfo.redomStr,
+                }).then(res => {
                     commit('SET_TOKEN', res.headers.token);
                     commit('DEL_ALL_TAG', []);
                     commit('CLEAR_LOCK');
-                    resolve();
-                })
+                    resolve(res);
+                });
             })
-
         },
         //根据手机号登录
         LoginByPhone({commit}, userInfo) {
             return new Promise((resolve) => {
-                loginByUsername(userInfo.phone, userInfo.code).then(res => {
-                    const data = res.data.data;
-                    commit('SET_TOKEN', data);
-                    commit('DEL_ALL_TAG');
+                crud.post("/api/admin/sys/user/login", {
+                    username: userInfo.phone,
+                    password: password,
+                    code: userInfo.code,
+                    redomStr: userInfo.redomStr,
+                }).then(res => {
+                    commit('SET_TOKEN', res.headers.token);
+                    commit('DEL_ALL_TAG', []);
                     commit('CLEAR_LOCK');
-                    resolve();
-                })
+                    resolve(res);
+                });
             })
         },
+        // 获取用户信息
         GetUserInfo({commit}) {
             return new Promise((resolve, reject) => {
-                getUserInfo().then((res) => {
+                crud.get("/api/admin/sys/user/findUser").then(res => {
                     const data = res.data.data;
-                    // commit('SET_USERIFNO', data.userInfo);
-                    // commit('SET_ROLES', data.roles);
-                    // commit('SET_PERMISSION', data.permission)
                     commit('SET_USERIFNO', data);
                     commit('SET_ROLES', data.roles);
-                    resolve(data);
-                }).catch(err => {
-                    reject(err);
-                })
-            })
-        },
-        //刷新token
-        RefeshToken({state, commit}) {
-            return new Promise((resolve, reject) => {
-                refeshToken(state.refeshToken).then(res => {
-                    const data = res.data.data;
-                    commit('SET_TOKEN', data);
-                    resolve(data);
-                }).catch(error => {
-                    reject(error)
-                })
+                    resolve(res);
+                });
             })
         },
         // 登出
@@ -91,7 +73,7 @@ const user = {
                 resolve()
             })
         },
-        //注销session
+        // 注销session
         FedLogOut({commit}) {
             return new Promise(resolve => {
                 commit('SET_TOKEN', '');
@@ -112,10 +94,15 @@ const user = {
          */
         GetTopMenu() {
             return new Promise(resolve => {
-                getTopMenu().then((res) => {
+                crud.get("/api/admin/sys/menu/list", {
+                    disable: 0,
+                    isTree: true,
+                    isLoginUser: true,
+                    isBottomLayer: false,
+                }).then(res => {
                     const data = res.data.data;
                     resolve(data)
-                })
+                });
             })
         },
         /**
@@ -129,33 +116,32 @@ const user = {
         GetMenu({commit}, item) {
             console.log("菜单获取", item);
             return new Promise(resolve => {
-                //item != null ? item.id : null
-                getMenu().then((res) => {
+                crud.get("/api/admin/sys/menu/list", {
+                    pid: item.id,
+                    disable: 0,
+                    isTree: true,
+                    isLoginUser: true,
+                }).then(res => {
                     let menus = res.data.data;
                     if (menus == null || menus.length === 0) {
-                        // 缓存左菜单
+                        // 清空
                         commit('SET_MENUALL', []);
                         commit('SET_MENU', []);
                         resolve([]);
-                        return;
-                    }
-                    //
-                    let newMenus = deepClone(menus);
-                    newMenus.forEach(ele => formatPath(ele, true));
-                    // 缓存所有菜单数据
-                    //setStore({name: 'xj-menus', content: newMenus})
-                    if (item != null && item.id) {
-                        for (let i = 0; i < newMenus.length; i++) {
-                            if (newMenus[i].id === item.id) {
-                                // 缓存左菜单
-                                commit('SET_MENUALL', newMenus[i].menus);
-                                commit('SET_MENU', newMenus[i].menus);
-                                resolve(newMenus[i].menus);
-                                break;
+                    } else {
+                        // 所有菜单
+                        let newMenus = deepClone(menus);
+                        newMenus.forEach(ele => formatPath(ele, true));
+                        // 默认左菜单展示索引0的菜单, 如果是点击的顶部菜单，存在pid,则左菜单展示对应pid的菜单
+                        let leftMenus = newMenus[0].menus;
+                        if (item != null && item.id) {
+                            for (let i = 0; i < newMenus.length; i++) {
+                                if (newMenus[i].id === item.id) {
+                                    leftMenus = newMenus[i].menus;
+                                }
                             }
                         }
-                    } else {
-                        // 缓存左菜单
+                        // 缓存左菜单并返回
                         commit('SET_MENUALL', newMenus[0].menus);
                         commit('SET_MENU', newMenus[0].menus);
                         resolve(newMenus[0].menus)
@@ -200,12 +186,6 @@ const user = {
         SET_ROLES: (state, roles) => {
             state.roles = roles;
         },
-        SET_PERMISSION: (state, permission) => {
-            state.permission = {};
-            permission.forEach(ele => {
-                state.permission[ele] = true;
-            });
-        }
     }
 };
 
