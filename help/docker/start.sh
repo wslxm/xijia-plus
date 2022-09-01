52fc4f0256a3682a480c03e3ae6f0546a7aad205
“wangsong” <1720696548@qq.com>
2022-09-01 19:47:44 +0800

    -更新脚本

--- help/docker/start.sh
+++ help/docker/start.sh
@@ -1,12 +1,12 @@
 ## 停止容器
-docker stop spring-boot-plus2
+docker stop xijia-plus
 ## 删除容器
-docker rm spring-boot-plus2
+docker rm xijia-plus
 ## 删除镜像
-docker rmi spring-boot-plus2:v2.x
+docker rmi xijia-plus:v2.x
 ## 创建镜像命令
-docker build -t spring-boot-plus2:v2.x .
+docker build -t xijia-plus:v2.x .
 ## 启动命令 (挂载时区 和 目录)
-docker run --name spring-boot-plus2 -v /etc/localtime:/etc/localtime -v /docker/spring-boot-plus2/server:/data -p 9049:9049 -d -it spring-boot-plus2:v2.x /bin/bash
+docker run --name xijia-plus -v /etc/localtime:/etc/localtime -v /docker/spring-boot-plus2/server:/data -p 9049:9049 -d -it xijia-plus:v2.x /bin/bash
 ## 查看启动状态,输出实时日志
-docker logs -f spring-boot-plus2
+docker logs -f xijia-plus
