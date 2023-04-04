b65174a668e7c5c484ec5d20348187e74c7d374e
“wangsong” <1720696548@qq.com>
2023-04-04 14:03:28 +0800

    错误处理

--- xj-base/xj-base-file/src/main/java/io/github/wslxm/springbootplus2/FileServer.java
+++ xj-base/xj-base-file/src/main/java/io/github/wslxm/springbootplus2/FileServer.java
@@ -1,11 +1,8 @@
 package io.github.wslxm.springbootplus2;
 
-import com.sun.xml.internal.bind.v2.util.DataSourceSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-import javax.sql.DataSource;
 
 /**
  * 文件服务  (无需启动)
